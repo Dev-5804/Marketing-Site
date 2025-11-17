ddb0e23d24b303530034683b2e71cdfb8a946d12
Dev-5804 <dev.endra.mali.5804@gmail.com>
2025-11-17 21:40:07 +0530

    Fixed React type error

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { cn } from "@/lib/cn";
 
 type ContainerProps = {
-  as?: keyof JSX.IntrinsicElements;
+  as?: keyof React.JSX.IntrinsicElements;
   className?: string;
   children: ReactNode;
 };
